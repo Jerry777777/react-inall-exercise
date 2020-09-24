24b8d730e745dc617a5cb222c3364bbcdd2fd039
Jerry777777 <491818175@qq.com>
2020-09-24 13:35:51 +0800

    [xiawenhao] feat: add calculator detail

--- src/app/App.js
+++ src/app/App.js
@@ -3,6 +3,7 @@ import './app.less';
 import {Route, BrowserRouter} from "react-router-dom";
 import Home from "./Home";
 import Header from './Header';
+import Calculator from './calculator/Calculator';
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
       <div className="app">
         <BrowserRouter>
           <Header />
-          <Route exact path='/' component={Home}/>
+          <Route exact path='/' component={Home} />
+          <Route path='/calculator' component={Calculator} />
         </BrowserRouter>
       </div>
     );
